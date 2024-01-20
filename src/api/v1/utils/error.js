32ec74abe44f98df19b3816a1ceacca319bd55c9
Rydozz15 <juan.fuentes@uc.cl>
2024-01-20 04:20:56 -0300

    Add a function and dictionary to find errors

---
+++ src/api/v1/utils/error.js
@@ -0,0 +1,20 @@
+const ERRORS = [
+  {
+    code: "23502",
+    status: 400,
+    message: "El campo destino o presupuesto no puede estar vacio",
+  },
+  {
+    code: "42P01",
+    status: 500,
+    message: "error en la conexion con la base de datos",
+  },
+  { code: "22P02", status: 400, message: "bad request" },
+  { code: "42601", status: 400, message: "errro de sintaxis en la consulta" },
+];
+
+const findError = (code) => {
+  return ERRORS.filter((err) => err.code == code);
+};
+
+export { findError };
